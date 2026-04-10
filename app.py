f79c9c9e79b13079e4f71a85e64c6f685be17d2d
Muhammad Umar <syedumarshah40@gmail.com>
2026-04-10 00:27:27 -0400

    Added CI/CD pipeline with SAST

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, redirect, session
+from flask_wtf import CSRFProtect, FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.secret_key = "secret123"  # weak secret
+app.secret_key = "supersecretkey123"
+
+# Enable CSRF
+csrf = CSRFProtect(app)
 
 # Fake database
 users = {
@@ -14,8 +20,22 @@ results = {
     "2": {"name": "Umar", "marks": 90}
 }
 
+user_result_map = {
+    "student1": "1",
+    "admin": "all"
+}
+
+# Form
+class EditForm(FlaskForm):
+    marks = StringField('Marks', validators=[DataRequired()])
+    submit = SubmitField('Update')
+
+
 @app.route('/', methods=['GET', 'POST'])
+@csrf.exempt
 def login():
+    error = None
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -23,33 +43,51 @@ def login():
         if username in users and users[username]["password"] == password:
             session['user'] = username
             return redirect('/dashboard')
-    return render_template('login.html')
+        else:
+            error = "Invalid Credentials"
+
+    return render_template('login.html', error=error)
 
 
 @app.route('/dashboard')
 def dashboard():
     if 'user' not in session:
         return redirect('/')
-    return render_template('dashboard.html', results=results)
+    return render_template('dashboard.html', results=results, user=session['user'])
 
 
-# 🚨 IDOR VULNERABILITY
-@app.route('/result/<id>')
+@app.route('/result/<id>', methods=['GET', 'POST'])
 def view_result(id):
     if 'user' not in session:
         return redirect('/')
 
-    # ❌ No authorization check
-    data = results.get(id)
-    return render_template('result.html', data=data, id=id)
+    username = session['user']
+
+    # IDOR FIX
+    if username != "admin" and user_result_map.get(username) != id:
+        return "Unauthorized Access", 403
+
+    form = EditForm()
+
+    
+    # Only admin can edit
+    if form.validate_on_submit():
+        if username != "admin":
+            return "Unauthorized Action", 403
+
+        results[id]['marks'] = form.marks.data
+        return redirect('/dashboard')
 
 
-# 🚨 No CSRF Protection
-@app.route('/edit/<id>', methods=['POST'])
-def edit_result(id):
-    new_marks = request.form['marks']
-    results[id]['marks'] = new_marks
-    return redirect('/dashboard')
+
+    return render_template('result.html', data=results[id], form=form)
+
+
+@app.after_request
+def security_headers(response):
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['Content-Security-Policy'] = "frame-ancestors 'none'"
+    return response
 
 
 if __name__ == '__main__':
